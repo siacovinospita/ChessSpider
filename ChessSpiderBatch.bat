cf77065d17a6a346fff653d56ebd78523a85248e
Sebastian Iacovino Spitaleri <s.iacovinospitaleri@student.tudelft.nl>
2024-12-18 15:28:03 +0100

    ignore and initial debugger

---
+++ ChessSpiderBatch.bat
@@ -0,0 +1,5 @@
+@echo off
+cd "C:\Projects\ChessSpider\src"
+prompt $g
+C:\Users\iacov\anaconda3\python.exe C:\Projects\ChessSpider\src\spider.py
+@echo on
--- src/chrome_driver.py
+++ src/chrome_driver.py
@@ -2,19 +2,45 @@ from config import config
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.chrome.options import Options
+import subprocess
 
-def start ():
+def start():
+
+
+    # Command to launch Chrome with a specific profile
+    command = [
+        # config['driver-path'],
+        "C:\Program Files\Google\Chrome\Application\chrome.exe",
+        # "chrome.exe",
+        f"--user-data-dir={config['user-data-dir']}",
+        f"--profile-directory={config['profile-directory']}",
+        "--remote-debugging-port=9222"
+    ]
+
+    print(command)
+
+
+    result = subprocess.Popen(
+        command,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        text=True
+    )
+
+    print("Output:", result.stdout)
+    print("Errors:", result.stderr)
+
     """
-    Configures and starts a Selenium Chrome WebDriver.
-    """
-    # Set up Chrome options.
+        Attaches Selenium to an already running Chrome instance.
+        """
     options = Options()
-    options.add_argument(f"--user-data-dir={config['user-data-dir']}")
-    options.add_argument(f"profile-directory={config['profile-directory']}")
+    options.debugger_address = "127.0.0.1:9222"  # Address of the remote debugging port
 
-    # Set up Chrome driver service.
-    service = Service(config['driver-path'])
+    # Attach to the existing session
+    driver = WebDriver(options=options)
 
     # Launch Chrome.
-    return webdriver.Chrome(service=service, options=options)
+    return driver
 
